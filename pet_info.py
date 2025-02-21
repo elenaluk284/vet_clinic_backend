673181246d843c8d91fe84a948c973168ffc1a35
elenaluk284 <elenaluk284@gmail.com>
2025-02-21 14:14:25 +0300

    Add a program for entering pet information

---
+++ pet_info.py
@@ -0,0 +1,6 @@
+pet_type = input("Введите вид питомца: ")
+pet_name = input("Введите кличку питомца: ")
+pet_age = input("Введите возраст питомца: ")
+
+output = f'Это {pet_type} по кличке "{pet_name}". Возраст: {pet_age} года.'
+print(output)
